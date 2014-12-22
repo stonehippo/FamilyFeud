a68593bfad099592cc669f30cdfe1a9e18ef7c1b
George White <stonehippo@gmail.com>
2014-12-21 23:46:59 -0500

    Got things started with a basic game file and loading logic

---
+++ js/FamilyFeud.js
@@ -0,0 +1,60 @@
+/*
+A simple Family Feud engine
+*/
+
+var FamilyFeud = (function () {
+	var that = {},
+		boards = [],
+		rounds = 0,
+		currentBoard = 0;
+		getBoards = function () {
+			return boards;
+		},
+		getRounds = function () {
+			return rounds;
+		},
+		drawBoard = function (board) {
+			var body = $('body');
+			if (typeof board.played === 'undefined') {
+				board.played = false;
+			}
+			body.append(["<div class='board'>",board.topic,"</div>"].join(""));	
+		},
+		dismissCurrentBoard = function () {
+			$('.board').remove();
+		},
+		nextBoard = function () {
+			if(currentBoard < rounds - 1) {
+				dismissCurrentBoard();
+				currentBoard++;
+				drawBoard(boards[currentBoard]);
+			}
+		},
+		previousBoard = function () {
+			if(currentBoard > 0) {
+				dismissCurrentBoard();
+				currentBoard--;
+				drawBoard(boards[currentBoard]);
+			}
+		},
+		start = function () {
+			drawBoard(boards[currentBoard]);
+		};
+
+	// load the data for the game
+	if (!boards.length) {
+		$.getJSON("/data.json", function(data) {
+			boards = data.boards;
+			rounds = boards.length;
+			start();
+		});
+	}
+
+
+	// Expose the public API - really only used for troulbeshooting
+	that.boards = getBoards;
+	that.rounds = getRounds;
+	that.nextBoard = nextBoard;
+	that.previousBoard = previousBoard;
+	return that;
+})();
